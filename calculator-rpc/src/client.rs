64eea1f161158115628ec8a2dd095604a8abbbb7
Lauri Gustafsson <lauri.gustafsson@iki.fi>
2024-11-13 11:17:41 +0200

    calculator/: add initial client and server

--- calculator-rpc/src/client.rs
+++ calculator-rpc/src/client.rs
@@ -1,3 +1,21 @@
-fn main() {
-    println!("Hello, world!");
+use calculator::calculator_client::CalculatorClient;
+use calculator::ComputeRequest;
+
+pub mod calculator {
+    tonic::include_proto!("calculator");
+}
+
+#[tokio::main]
+async fn main() -> Result<(), Box<dyn std::error::Error>> {
+    let mut client = CalculatorClient::connect("http://[::1]:50051").await?;
+
+    let request = tonic::Request::new(ComputeRequest {
+        expression: "1 + 1".into(),
+    });
+
+    let result = client.compute(request).await?;
+    let res = &result.get_ref().result;
+    println!("Server returned result: {res}");
+
+    Ok(())
 }
--- calculator-rpc/src/server.rs
+++ calculator-rpc/src/server.rs
@@ -1,3 +1,40 @@
-fn main() {
-    println!("Hello, world!");
+use tonic::{transport::Server, Request, Response, Status};
+
+use calculator::calculator_server::{Calculator, CalculatorServer};
+use calculator::{ComputeRequest, ComputeResult};
+
+pub mod calculator {
+    tonic::include_proto!("calculator");
+}
+
+#[derive(Debug, Default)]
+struct MyCalculator {}
+
+#[tonic::async_trait]
+impl Calculator for MyCalculator {
+    async fn compute(
+        &self,
+        request: Request<ComputeRequest>,
+    ) -> Result<Response<ComputeResult>, Status> {
+        println!("Got a compute request: {request:?}");
+
+        let reply = ComputeResult {
+            result: "42".into(),
+        };
+
+        Ok(Response::new(reply))
+    }
+}
+
+#[tokio::main]
+async fn main() -> Result<(), Box<dyn std::error::Error>> {
+    let addr = "[::1]:50051".parse()?;
+    let calculator = MyCalculator::default();
+
+    Server::builder()
+        .add_service(CalculatorServer::new(calculator))
+        .serve(addr)
+        .await?;
+
+    Ok(())
 }
