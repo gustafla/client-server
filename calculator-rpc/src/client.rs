3bc492e50a3e5b8ef0ae2f937e36f111b1262f33
Lauri Gustafsson <lauri.gustafsson@iki.fi>
2024-11-13 11:37:34 +0200

    calculator/client.rs: Add user input line reader

--- calculator-rpc/src/client.rs
+++ calculator-rpc/src/client.rs
@@ -1,5 +1,6 @@
 use calculator::calculator_client::CalculatorClient;
 use calculator::ComputeRequest;
+use tokio::io::{self, AsyncBufReadExt, BufReader};
 
 pub mod calculator {
     tonic::include_proto!("calculator");
@@ -8,14 +9,16 @@ pub mod calculator {
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn std::error::Error>> {
     let mut client = CalculatorClient::connect("http://[::1]:50051").await?;
+    let stdin = BufReader::new(io::stdin());
 
-    let request = tonic::Request::new(ComputeRequest {
-        expression: "1 + 1".into(),
-    });
+    let mut lines = stdin.lines();
+    while let Some(expression) = lines.next_line().await? {
+        let request = tonic::Request::new(ComputeRequest { expression });
 
-    let result = client.compute(request).await?;
-    let res = &result.get_ref().result;
-    println!("Server returned result: {res}");
+        let result = client.compute(request).await?;
+        let res = &result.get_ref().result;
+        println!("Server returned result: {res}");
+    }
 
     Ok(())
 }
